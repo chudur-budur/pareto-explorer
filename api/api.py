2c99c4905c4daa3f9b00488a65f0cc75472856ac
chudur-budur <khaled.ahsan.talukder@gmail.com>
2020-10-10 03:51:33 -0400

    adding docstring

--- api/api.py
+++ api/api.py
@@ -1,3 +1,23 @@
+"""api.py -- A RESTful API to connect to `pviz` through flask
+    This module provides a RESTful API to connect to `pviz` [1]_ 
+    capabilities through a `flask` service endpoint. This module
+    serves as the backend to the `pareto-explorer` app.
+
+    Copyright (C) 2016
+    Computational Optimization and Innovation (COIN) Laboratory
+    Department of Computer Science and Engineering
+    Michigan State University
+    428 S. Shaw Lane, Engineering Building
+    East Lansing, MI 48824-1226, USA
+    
+    References
+    ----------
+    .. [1] [pviz](https://github.com/chudur-budur/pviz)
+
+.. moduleauthor:: AKM Khaled Talukder <talukde1@msu.edu>
+"""
+
+
 import time
 import os
 import json
@@ -8,6 +28,23 @@ app = Flask(__name__)
 
 
 def make_path(pf):
+    r"""Builds a path from the route parameter string
+
+    Builds a relative path from the route parameter string.
+    The route parameters are basically the name of the Pareto-front data 
+    to be loaded, i.e. `'cdebmk-nbi-4d'`. This function breaks it up and 
+    constructs a path like `./data/cdebmdk-nbi/4d/index.js`
+
+    Parameters
+    ----------
+    pf : str 
+        A route parameter string.
+
+    Returns
+    -------
+    path : str or os.path
+        The path to data.
+    """
     parts = pf.split('-')
     dir_name = '-'.join(parts[:-1])
     dim = parts[-1]
@@ -16,6 +53,21 @@ def make_path(pf):
 
 
 def parse_json(path):
+    r"""Parses a javascript object into json string
+
+    Parses the javascript object pointed by the `path` string variable
+    and returns a json string to be dispatched by the api endpoint.
+
+    Parameters
+    ----------
+    path : str or os.path
+        Path to the Pareto-front data file as javascript object.
+
+    Returns
+    -------
+    jstr : str
+        JSON string of the javascript object given in the file.
+    """
     fp = open(path)
     jstr = ''
     for line in fp:
@@ -42,10 +94,26 @@ def parse_json(path):
     return jstr
 
 
-@app.route('/api/<string:pf>')
-def get_pf(pf):
-    print("Received request for " + pf)
-    path = make_path(pf)
+@app.route('/api/<string:pareto_front>')
+def get_pf(pareto_front):
+    r"""An API endpoint to dispatch Pareto-front data
+
+    This function receives the name of the Pareto-front data to be loaded
+    by the pareto-explorer app. Then it loads the javascript object
+    from the relevant path.
+
+    Parameters
+    ----------
+    pareto_front : str 
+        The API endpoint parameter in string.
+
+    Returns
+    -------
+    data : str
+        A JSON string representation of the javascript object.
+    """
+    print("Received request for " + pareto_front)
+    path = make_path(pareto_front)
     print("Fetching " + path + ' ...')
     data = 'null'
     if os.path.exists(path):
@@ -56,9 +124,5 @@ def get_pf(pf):
 
 
 if __name__ == "__main__":
-    t = get_current_time()
-    print(t)
-    n = get_name()
-    print(n)
     d = get_pf('cdebmdk-nbi-4d')
     print(d)
